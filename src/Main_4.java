d1f34279229df76399d0d6a501009760d35f8a7a
Денис <ivanovds87@mail.ru>
2022-10-28 23:48:59 +0300

    Задача 4 - fin

---
+++ src/Main_4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main_4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String resp = "Заархивированный вирус";
+        String help = "Подсказка";
+        String helpText = "Зазипованная вредоносная программа";
+        String noHelpText = "Подсказка уже недоступна";
+        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+
+        for (int attempts = 3; attempts > 0; attempts--) {
+            String test = sc.nextLine();
+
+            if (test.compareTo(resp)==0){
+                System.out.println("Правильно!");
+                System.exit(0);}
+            else if (test.compareTo(help)==0&attempts == 3) {
+                System.out.println(helpText);
+                attempts = 2;
+            }
+            else if (test.compareTo(help)==0&attempts != 3) {
+                System.out.println(noHelpText);
+                attempts++;
+            }
+            else {
+                if (attempts == 1) {
+                    System.out.println("Обидно, приходи в другой раз");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Подумай еще!");
+                }
+            }
+        }
+    }
+
+}
+
+
